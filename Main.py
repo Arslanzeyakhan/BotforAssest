fa2e5df8cdc49f874d78be947300db016a790a03
Arslanzeyakhan <74643255+Arslanzeyakhan@users.noreply.github.com>
2022-04-22 21:14:15 +0530

    Main.py

---
+++ Main.py
@@ -0,0 +1,11 @@
+name = input('What is your name?\n')
+print('Hey, %s.' % name)
+country = input('what country do you live in ? \n')
+print('i love %s.' % country)
+hobbie = input('what is you"re Favriote activity ?\n')
+print('Hey ,  Bingo i like %s too.' % hobbie)
+
+# Every thing can be replaced and  1 or 2 thousand lines would be great to put this together
+
+Bye = input('Ok well looks like Times out Bye for now \n')
+print('Ok bye thanks for spending time with me \n')
